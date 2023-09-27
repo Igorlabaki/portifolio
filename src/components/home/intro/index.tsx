fb7c1669b71753bb5521457e29ee561baf692f70
Igor Goncalo <igorlabakig@gmail.com>
2023-09-27 20:42:53 +0100

    fix home image

--- src/components/home/intro/index.tsx
+++ src/components/home/intro/index.tsx
@@ -11,15 +11,15 @@ export default function IntroComponent() {
                 <p className='text-[12px] text-custom-yellow w-fit rotate-[-90deg] bottom-[42px] absolute left-[-36px]'>product designer</p>
             </div>
             <div className='absolute right-0 top-0'>
-                <ImageComponent alt="manWalking" w="w-[516px]" h="h-[687px]" src={"/images/manWalking.png"} containerClassname="brightness-125"/>
+                <ImageComponent alt="manWalking" w="w-[516px]" h="h-[687px]" src={"/images/home/manWalking.png"} containerClassname="brightness-125"/>
                 <div className="right-[47px] bottom-[251px] flex justify-between items-center w-[120px] h-[105px] absolute">
-                    <ImageComponent  alt='leftBox'  w="w-[27px]" h="h-[102px]" src={"/images/left.png"} /> 
-                    <ImageComponent  alt='ritghBox'  w="w-[27px]" h="h-[102px]" src={"/images/rigth.png"} /> 
+                    <ImageComponent  alt='leftBox'  w="w-[27px]" h="h-[102px]" src={"/images/home/left.png"} /> 
+                    <ImageComponent  alt='ritghBox'  w="w-[27px]" h="h-[102px]" src={"/images/home/rigth.png"} /> 
                 </div>
             </div>
             <div className='absolute right-0 top-0 z-40'>
                 <div className='w-[516px] h-[687px] relative'>
-                <Image src={"/images/texture.png"} className='w-full h-full' alt='ritghBox' layout="fill" objectFit={'cover'}/>
+                <Image src={"/images/home/texture.png"} className='w-full h-full' alt='ritghBox' layout="fill" objectFit={'cover'}/>
                 </div>
             </div>
             <div className="absolute bottom-1 right-0 ">
