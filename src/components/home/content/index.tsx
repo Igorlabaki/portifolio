e4268018a914c4657a00128551569084d4218a8f
Igor Goncalo <igorlabakig@gmail.com>
2023-10-02 19:35:13 +0100

    upadte

--- src/components/home/content/index.tsx
+++ src/components/home/content/index.tsx
@@ -62,7 +62,7 @@ export default function ContentComponent() {
         </AnimatedVerticalComponent>
 
           <motion.div initial={{y: 300}} animate={{y:animation ? 0 : 300}} transition={{duration: 1.3}}>
-            <ItemGridComponent  width="relative w-[820px] " heigth="h-[289px]" subTitle="  Mercedes Benz" title="UI Concepts">
+            <ItemGridComponent  width="relative w-[820px] " heigth="h-[289px]  translate-y-[-303px]" subTitle="  Mercedes Benz" title="UI Concepts">
               <ImageComponent containerClassname="" alt="foto" w="w-[820px] " h="h-[289px]" src={"/images/home/home-image-6.png"}/> 
             </ItemGridComponent>
           </motion.div>
