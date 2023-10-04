ed721aa079121917b6446f93758f6bd7022cd763
Igor Goncalo <igorlabakig@gmail.com>
2023-10-04 08:52:07 +0100

    update

--- src/components/header/headerItem.tsx
+++ src/components/header/headerItem.tsx
@@ -1,17 +1,18 @@
 'use client'
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 
 interface HeaderProps{
     text: string
 }
 export function HeaderItemComponent({text}:HeaderProps) {
   const pathname = usePathname()
+  const {replace} = useRouter()
   const redirect = text.toLowerCase() === "home" ? "/" : text.toLowerCase()
   const homeBgChange = text.toLowerCase() === "home" && pathname === "/"
   const BgChange = `/${text.toLowerCase()}` === pathname 
-
+  console.log(redirect)
   return (
-    <Link href={redirect}  className={`text-[10px] w-[185px] h-[24px] font-wtGothic py-[6px] px-[10px] ${homeBgChange || BgChange ? "text-black bg-custom-white" : "text-custom-yellow bg-custom-black"}`}>{text}</Link>
+    <button onClick={ () => replace(`/${redirect}`)}  className={`text-[10px] w-[185px] h-[24px] font-wtGothic py-[6px] px-[10px] ${homeBgChange || BgChange ? "text-black bg-custom-white" : "text-custom-yellow bg-custom-black"}`}>{text}</button>
   )
 }
